ef2f4228929064f7d6841fce7c713ac34d5c409b
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-10-14 09:34:34 +0300

    setting database connection

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 const PORT = 5000;
+
+//connect to database
+connectDB();
 
 app.listen(process.env.PORT||PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
