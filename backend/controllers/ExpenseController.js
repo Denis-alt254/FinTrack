a01290999b7e98720eb073293a1844adf2a5cbe9
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-10-15 14:16:09 +0300

    Editing Expense controller

--- backend/controllers/ExpenseController.js
+++ backend/controllers/ExpenseController.js
@@ -19,3 +19,22 @@ const AddExpense = async(req, res) => {
         res.status(500).json({error: error.message});
     }
 }
+
+const EditExpense = async(req, res) => {
+    try {
+        const userId = req.user.userId;
+        const expense = await Expense.findByIdAndUpdate({userId});
+        if(!expense){
+            return res.status(404).json("Expense not found.")
+        }
+        const {title, amount, category, date, notes} = req.body;
+        const updatedExpense = expense({title, amount, category, date, notes});
+        const savedExpense = await updatedExpense.save();
+        res.status(200).json(savedExpense);
+    } catch (error) {
+        console.error("Error editing expense.", error);
+        res.status(500).json({error: error.message});
+    }
+}
+
+module.exports = {AddExpense, EditExpense};
