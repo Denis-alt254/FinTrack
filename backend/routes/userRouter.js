24de76a25a2e7f5ea7983d13f6adde088bd68c65
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-10-15 15:41:17 +0300

    user router updated

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { Register, Login } = require("../controllers/AuthController");
+const { Register, Login, getAllUsers } = require("../controllers/AuthController");
 const router = express.Router();
 
 router.post('/register', Register);
 router.post('/login', Login);
+router.get('/', getAllUsers);
 
 module.exports = router;
