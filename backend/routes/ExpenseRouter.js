88f5ed57a312fbaa0c4ee90b66526bc4b150dc1c
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-10-21 10:51:58 +0300

    delete routes

--- backend/routes/ExpenseRouter.js
+++ backend/routes/ExpenseRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { GetAllExpense, AddExpense, EditExpense } = require('../controllers/ExpenseController');
+const { GetAllExpense, AddExpense, EditExpense, DeleteExpense } = require('../controllers/ExpenseController');
 const protect = require('../middleware/AuthMiddleware');
 const router = express.Router();
 
 router.get('/', protect, GetAllExpense);
 router.post('/', protect, AddExpense);
 router.put('/:id', protect, EditExpense);
+router.delete('/:id', DeleteExpense);
 
 module.exports = router;
