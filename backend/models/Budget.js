213aa2d6d1cc24e0faa7208d89d55955dba2d641
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-10-21 12:20:50 +0300

    modified budget schema

--- backend/models/Budget.js
+++ backend/models/Budget.js
@@ -1,11 +1,30 @@
 const { default: mongoose } = require("mongoose");
 
 const BudgetSchema = new mongoose.Schema({
-    userId: [{type: mongoose.Schema.Types.ObjectId, ref:"User"}],
-    category: String,
-    limit: Number,
-    month: Number,
-    year: Number
+    userId: {type: mongoose.Schema.Types.ObjectId, ref:"User"},
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    limit: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    month: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 12
+    },
+    year: {
+        type: Number,
+        required: true
+    }
+
 }, {timestamps: true});
+
+BudgetSchema.index({userId: 1, category: 1, limit: 1, month: 1, year: 1}, {unique: true});
 
 module.exports = mongoose.model("Budget", BudgetSchema);
