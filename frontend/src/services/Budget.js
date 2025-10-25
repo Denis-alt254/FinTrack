6e6122db72fcdc6a5218527651879ac5eb3d2b79
Denis-alt254 <deniskipkurui2024@gmail.com>
2025-10-25 16:45:11 +0300

    Budget frontend apis

---
+++ frontend/src/services/Budget.js
@@ -0,0 +1,10 @@
+import API from "./api"
+
+export const SetBudget = async(data) => {
+    const res = await API.post('/', data);
+    return res.data;
+}
+
+export const CheckBudgetUsage = async() => {
+    return await API.get('/');
+}
